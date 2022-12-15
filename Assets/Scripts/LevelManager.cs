0ef33949a521dba9efd07eabb1f97d0869f4579b
Weerawat Lappermthawee <weerawat.fo@mail.kmutt.ac.th>
2022-12-15 17:32:04 +0700

    create | start & finish level with obstacle

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -11,18 +11,26 @@ public class LevelManager : MonoBehaviour
     [Header("Levels")]
     public GameObject[] levels;
     GameObject currentLevelObj;
-    GameObject currentLevelStartLine;
-    GameObject currentLevelFinishLine;
+    Level currentLevel;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        currentLevelObj = levels[0];
+        currentLevel = currentLevelObj.GetComponent<Level>();
+        Vector3 startPos = currentLevel.startLine.transform.position;
+        player.transform.position = new Vector3(startPos.x, startPos.y + 0.5f, startPos.z);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentLevel.IsFinish(player))
+        {
+            Debug.Log("You finished!!");
+            player.GetComponent<TestController>().enabled = false;
+        }
     }
 
     public void StartLevel()
