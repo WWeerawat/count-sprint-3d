62fb29beed783c47377dc98157a59e1c48eaadb2
Weerawat Lappermthawee <weerawat.fo@mail.kmutt.ac.th>
2022-12-18 00:40:14 +0700

    create | first refactor

--- Assets/Scripts/LevelState/LevelStateManager.cs
+++ Assets/Scripts/LevelState/LevelStateManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Cinemachine;
 using UnityEngine;
 
 namespace LevelState
@@ -6,7 +7,22 @@ namespace LevelState
     public class LevelStateManager : MonoBehaviour
     {
         private LevelBaseState currentState;
-        private InitiateLevelState initiateLevelState = new InitiateLevelState();
+        public InitiateLevelState initiateLevelState = new InitiateLevelState();
+        public PlayLevelState playLevelState = new PlayLevelState();
+        public ResultLevelState resultLevelState = new ResultLevelState();
+
+        [Header("Utils")]
+        public GameObject player;
+
+        [Header("Levels")]
+        public GameObject[] levels;
+        public GameObject currentLevelObj;
+        public Level.Level currentLevel;
+        public int currentCount;
+
+        public GameObject spawnedPlayer;
+
+        public CinemachineVirtualCamera cinemachineVirtualCamera;
 
         private void Start()
         {
@@ -22,6 +38,40 @@ namespace LevelState
         {
             currentState = state;
             currentState.EnterState(this);
+        }
+
+        public void StartLevel()
+        {
+            InstantiatePlayer();
+            GameManager.Instance.mainMenuUI.SetActive(false);
+        }
+
+        // public void SetToStartPosition()
+        // {
+        //     Vector3 startPos = currentLevel.startLine.transform.position;
+        //     player.transform.position = new Vector3(startPos.x, startPos.y + 0.5f, startPos.z);
+        // }
+        public void ResetLevel()
+        {
+            Debug.Log("YOU DIED");
+            DestroyPlayer();
+            InstantiatePlayer();
+        }
+
+        private void InstantiatePlayer()
+        {
+            Vector3 startPos = currentLevel.startLine.transform.position;
+            spawnedPlayer = Instantiate(player, new Vector3(startPos.x, startPos.y + 0.5f, startPos.z), Quaternion.identity);
+            cinemachineVirtualCamera.Follow = spawnedPlayer.transform;
+        }
+        public void DestroyPlayer()
+        {
+            Destroy(spawnedPlayer);
+        }
+
+        public GameObject GetSpawnPlayer()
+        {
+            return spawnedPlayer;
         }
     }
 }
